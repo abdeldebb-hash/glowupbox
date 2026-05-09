7957d35f03755d1578d064b5ace16214b809ec20
abdeldebb-hash <abdeldebb-hash@github.com>
2026-05-09 12:30:25 +0100

    Use @libsql/client/http for Vercel serverless compatibility

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,14 @@
-import { PrismaClient }      from '@prisma/client'
-import { PrismaLibSQL }       from '@prisma/adapter-libsql'
-import { createClient }       from '@libsql/client'
+import { PrismaClient } from '@prisma/client'
 
 function createPrisma() {
   const url   = process.env.TURSO_DATABASE_URL ?? ''
   const token = process.env.TURSO_AUTH_TOKEN   ?? ''
 
   if (url.startsWith('libsql://')) {
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const { createClient } = require('@libsql/client/http')
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const { PrismaLibSQL } = require('@prisma/adapter-libsql')
     const libsql  = createClient({ url, authToken: token })
     const adapter = new PrismaLibSQL(libsql)
     return new PrismaClient({ adapter } as never)
